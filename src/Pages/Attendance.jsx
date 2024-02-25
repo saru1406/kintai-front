73f1565aa29d88c72aa23d332f8554bc7f6b4599
saru1406 <y.s1406@ezweb.ne.jp>
2024-02-26 03:27:29 +0900

    休憩初期値取得 休憩終了保存 フラッシュメッセージ

--- src/Pages/Attendance.jsx
+++ src/Pages/Attendance.jsx
@@ -4,9 +4,12 @@ import { useNavigate } from "react-router-dom";
 
 const Attendance = () => {
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
-  const [remarks, setRemarks] = useState('')
+  const [remarks, setRemarks] = useState("");
+  const [isOnBreak, setIsOnBreak] = useState(false);
+  const [data, setData] = useState(null);
   const weekday = ["日", "月", "火", "水", "木", "金", "土"];
   const apiUrl = import.meta.env.VITE_APP_API_URL;
+  const authToken = sessionStorage.getItem("authToken");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -14,6 +17,26 @@ const Attendance = () => {
     }, 1000);
     return () => clearInterval(intervalId);
   }, []);
+
+  useEffect(() => {
+    const fetchBreakStatus = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/api/work/break-status`, {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+        console.log(response.data);
+        setIsOnBreak(response.data.break_status);
+        setData(response.data);
+      } catch (error) {
+        console.log(error.data.message);
+      }
+    };
+
+    fetchBreakStatus();
+  }, [apiUrl, authToken]);
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -25,16 +48,19 @@ const Attendance = () => {
 
   const jpDateString = currentDateTime.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
 
-  const authToken = sessionStorage.getItem("authToken");
-
   const handleWorkStart = async () => {
     try {
-      await axios.post(`${apiUrl}/api/work/start`, { remarks: remarks, start_date: jpDateString }, {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      });
+      const response = await axios.post(
+        `${apiUrl}/api/work/start`,
+        { remarks: remarks, start_date: jpDateString },
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      setData(response.data);
     } catch (error) {
       console.error(error.response.data.message);
     }
@@ -42,33 +68,64 @@ const Attendance = () => {
 
   const handleWorkEnd = async () => {
     try {
-      await axios.post(`${apiUrl}/api/work/end`, { remarks: remarks, end_date: jpDateString }, {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      });
+      const response = await axios.post(
+        `${apiUrl}/api/work/end`,
+        { remarks: remarks, end_date: jpDateString },
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      setData(response.data);
     } catch (error) {
       console.error(error.response.data.message);
     }
-  }
+  };
 
-  const handleWorkBreak = async () => {
+  const handleBreakStart = async () => {
     try {
-      await axios.post(`${apiUrl}/api/work/break-start`, { remarks: remarks, break_start: jpDateString }, {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${authToken}`
-        },
-      });
+      const response = await axios.post(
+        `${apiUrl}/api/work/break-start`,
+        { remarks: remarks, break_start: jpDateString },
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      setIsOnBreak(true);
+      setData(response.data);
     } catch (error) {
-      console.error(error.response.data.message)
+      console.error(error.response.data.message);
     }
-  }
+  };
+
+  const handleBreakEnd = async () => {
+    try {
+      const response = await axios.post(
+        `${apiUrl}/api/work/break-end`,
+        { remarks: remarks, break_end: jpDateString },
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      setIsOnBreak(false);
+      setData(response.data);
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
 
   return (
     <>
       <section className="text-gray-400 bg-gray-900 body-font overflow-hidden h-screen">
+      {data && <div className="bg-emerald-300 mt-auto text-center text-black">{data.message}</div>}
         <div className="container mx-auto">
           <div className="mt-60" />
           <div className="mx-auto text-white p-10 w-3/5 border rounded">
@@ -86,7 +143,9 @@ const Attendance = () => {
                 value={remarks}
                 onChange={handleChange}
                 className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              >{remarks}</textarea>
+              >
+                {remarks}
+              </textarea>
             </div>
             <div className="text-center">
               <button
@@ -101,63 +160,25 @@ const Attendance = () => {
               >
                 退勤
               </button>
-              <button
-                onClick={handleWorkBreak}
-                className="text-white bg-amber-500 border-0 py-2 px-8 focus:outline-none hover:bg-amber-600 rounded text-lg mt-10 mx-10"
-              >
-                休憩開始
-              </button>
+              {!isOnBreak && (
+                <button
+                  onClick={handleBreakStart}
+                  className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg mt-10 mx-10"
+                >
+                  休憩開始
+                </button>
+              )}
+              {isOnBreak && (
+                <button
+                  onClick={handleBreakEnd}
+                  className="text-white bg-amber-500 border-0 py-2 px-8 focus:outline-none hover:bg-amber-600 rounded text-lg mt-10 mx-10"
+                >
+                  休憩終了
+                </button>
+              )}
             </div>
           </div>
         </div>
-        {/* <div className="container px-5 py-24 mx-auto">
-          <div className="-my-8 divide-y-2 divide-gray-800">
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-white">
-                  CATEGORY
-                </span>
-                <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-white title-font mb-2">
-                  Bitters hashtag waistcoat fashion axe chia unicorn
-                </h2>
-                <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
-                </p>
-                <a className="text-indigo-400 inline-flex items-center mt-4">
-                  Learn More
-                </a>
-              </div>
-            </div>
-            <div className="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-white">
-                  CATEGORY
-                </span>
-                <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-white title-font mb-2">
-                  Meditation bushwick direct trade taxidermy shaman
-                </h2>
-                <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
-                </p>
-                <a className="text-indigo-400 inline-flex items-center mt-4">
-                  Learn More
-                </a>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </section>
     </>
   );
